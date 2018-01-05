bb3355e44b76e163e3b2bd9d2870135cb91ffe4d
Bhaskar Nair <b.bhaskar.nair@gmail.com>
2018-01-05 21:52:17 +0530

    Some Changes

---
+++ JS/MyJSEffects.js
@@ -0,0 +1,18 @@
+var slideIndex = 0;
+showSlides();
+var vals=["100","50","30","200","60"];
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+        $("#tm").text(vals[0]);
+
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {
+        slideIndex = 1
+    }
+    slides[slideIndex - 1].style.display = "block";
+    setTimeout(showSlides, 5000); // Change image every 5 seconds
+}
