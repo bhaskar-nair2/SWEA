0a22475aa3557c2a95a388f3b3a540bef2ae6055
Bhaskar Nair <b.bhaskar.nair@gmail.com>
2018-01-05 22:25:30 +0530

    Update MyJSEffects.js

--- JS/MyJSEffects.js
+++ JS/MyJSEffects.js
@@ -1,17 +1,29 @@
 var slideIndex = 0;
+var imgIndex = 0;
 showSlides();
-var vals=["100","50","30","200","60"];
+
 function showSlides() {
     var i;
+    //CS-fifa-pubg-cl-blur
+    var Tvals=["100","50","30","200","60"];
+    var Pvals=["100","100","100","50","100"];
+    var PZvals=["10K","3K","50K","1K","2K"];
     var slides = document.getElementsByClassName("mySlides");
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
-        $("#tm").text(vals[0]);
 
     }
     slideIndex++;
+    $("#tm").text(Tvals[imgIndex]);
+    $("#pm").text(Pvals[imgIndex]);
+    $("#pz").text(PZvals[imgIndex]);
+    imgIndex++;
+
     if (slideIndex > slides.length) {
         slideIndex = 1
+    }
+    if (imgIndex>4){
+        imgIndex=0
     }
     slides[slideIndex - 1].style.display = "block";
     setTimeout(showSlides, 5000); // Change image every 5 seconds
